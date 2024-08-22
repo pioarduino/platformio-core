d7ec4aff60f39f51fc1490193a5e39c9156869fe
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-22 12:52:26 +0200

    style format

--- platformio/package/manager/core.py
+++ platformio/package/manager/core.py
@@ -40,7 +40,11 @@ def get_core_package_dir(name, spec=None, auto_install=False):
         pkg_dir = pm.get_package(name).path
     except:
 # pylint: disable=raise-missing-from
-        raise exception.PlatformioException("Maybe missing entry(s) in platformio.ini ?\nPlease add  \"check_tool = cppcheck\" to use code check tool.\nIn all cases please restart VSC/PlatformIO to try to auto fix issues.")
+        raise exception.PlatformioException(
+            "Mavbe missing entry(s) in platformio.ini ?\n"
+            "Please add  \"check_tool = cppcheck\" to use code check tool.\n"
+            "In all cases please restart VSC/PlatformIO to try to auto fix issues."
+        )
 # pylint: enable=raise-missing-from
     return pkg_dir
 
