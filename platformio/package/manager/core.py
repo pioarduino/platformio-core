4ca816c10e4be8972ac2beef960673fe0366f3e7
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-08-25 18:21:28 +0200

    Install packages via IDF Installer

--- platformio/package/manager/core.py
+++ platformio/package/manager/core.py
@@ -13,10 +13,13 @@
 # limitations under the License.
 
 import os
+import subprocess
+import sys
+import shutil
 import tarfile
 
 from urllib import request
-from os.path import join
+from os.path import isfile, join
 
 from platformio import exception
 from platformio.dependencies import get_core_dependencies
@@ -24,6 +27,7 @@ from platformio.package.exception import UnknownPackageError
 from platformio.package.manager.tool import ToolPackageManager
 from platformio.project.config import ProjectConfig
 from platformio.package.meta import PackageSpec
+from platformio.proc import get_pythonexe_path
 
 
 def get_installed_core_packages():
@@ -39,7 +43,9 @@ def get_installed_core_packages():
 
 def get_core_package_dir(name, spec=None, auto_install=True):
     # pylint: disable=unused-argument
+    IDF_TOOLS_PATH_DEFAULT = os.path.join(os.path.expanduser("~"), ".espressif")
     pm = ToolPackageManager()
+    python_exe = get_pythonexe_path()
     base_pack_dir = ProjectConfig.get_instance().get("platformio", "packages_dir")
 
     if name in ("tool-scons", "tool-install") and not (os.path.exists(join(base_pack_dir, "tool-scons")) and os.path.exists(join(base_pack_dir, "tool-install"))):
@@ -72,15 +78,28 @@ def get_core_package_dir(name, spec=None, auto_install=True):
     try:
         if "tool-scons" in name:
             pkg_dir = pm.get_package("tool-scons").path
+            print("SUCCESS!! tool install", pkg_dir)
             assert pm.install("tool-scons")
     except Exception: # pylint: disable=broad-except
+        print("FAIL!!! install from", name)
         return
 
     try:
         if "tool-install" in name:
             pkg_dir = pm.get_package("tool-install").path
+            print("SUCCESS!! tool install", pkg_dir)
             assert pm.install("tool-install")
+            IDF_TOOLS = os.path.join(pkg_dir, "tools", "idf_tools.py")
+            IDF_TOOLS_FLAG = ["install"]
+            IDF_TOOLS_CMD = [python_exe, IDF_TOOLS] + IDF_TOOLS_FLAG
+            if not os.path.exists(join(IDF_TOOLS_PATH_DEFAULT, "tools")):
+                rc = subprocess.call(IDF_TOOLS_CMD)
+                if rc != 0:
+                    sys.stderr.write("Error: Couldn't execute 'idf_tools.py install' \n")
+                else:
+                    shutil.copytree(join(IDF_TOOLS_PATH_DEFAULT, "tools", "tool-packages"), join(IDF_TOOLS_PATH_DEFAULT, "tools"), symlinks=False, ignore=None, ignore_dangling_symlinks=False, dirs_exist_ok=True)
     except Exception: # pylint: disable=broad-except
+        print("FAIL!!! install from", name)
         return
 
     return pkg_dir
