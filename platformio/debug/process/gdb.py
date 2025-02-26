4d83f2cbf247911cc646223f437bd04c6fec8147
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-02-27 00:06:13 +0100

    Pioarduino (#13)
    * remove telemetry
    * no full git clone
    * add intelhex as required
    * no core packages
    * add `argcomplete` as pip dependencies
    * remove advertisings
    * add intelhex as required
    * install scons from github

--- platformio/debug/process/gdb.py
+++ platformio/debug/process/gdb.py
@@ -16,10 +16,8 @@ import os
 import signal
 import time
 
-from platformio import telemetry
 from platformio.compat import aio_get_running_loop, is_bytes
 from platformio.debug import helpers
-from platformio.debug.exception import DebugInitError
 from platformio.debug.process.client import DebugClientProcess
 
 
@@ -130,7 +128,6 @@ class GDBClientProcess(DebugClientProcess):
         self._handle_error(data)
         # go to init break automatically
         if self.INIT_COMPLETED_BANNER.encode() in data:
-            telemetry.log_debug_started(self.debug_config)
             self._auto_exec_continue()
 
     def console_log(self, msg):
@@ -175,7 +172,4 @@ class GDBClientProcess(DebugClientProcess):
             and b"Error in sourced" in self._errors_buffer
         ):
             return
-        telemetry.log_debug_exception(
-            DebugInitError(self._errors_buffer.decode()), self.debug_config
-        )
         self.transport.close()
