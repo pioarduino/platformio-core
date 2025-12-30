a85264e5cc3ee2f2439921cd1077aaac3803c2ee
Ivan Kravets <me@ikravets.com>
2025-12-30 12:06:52 +0200

    Update deps

--- platformio/dependencies.py
+++ platformio/dependencies.py
@@ -43,7 +43,7 @@ def get_pip_dependencies():
         # PIO Home requirements
         "ajsonrpc == 1.2.*",
         "starlette >=0.19, <0.51",
-        "uvicorn >=0.16, <0.39",
+        "uvicorn >=0.16, <0.41",
         "wsproto == 1.*",
     ]
 
