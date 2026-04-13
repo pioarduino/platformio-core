f58b4ecf15d41e929388707eed93709b458ab63e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2026-04-13 15:40:20 +0200

    updates

--- platformio/dependencies.py
+++ platformio/dependencies.py
@@ -47,8 +47,8 @@ def get_pip_dependencies():
     home = [
         # PIO Home requirements
         "ajsonrpc == 1.2.*",
-        "starlette >=0.19, <0.53",
-        "uvicorn >=0.16, <0.41",
+        "starlette < 1",
+        "uvicorn >=0.16, <0.43",
         "wsproto == 1.*",
     ]
 
